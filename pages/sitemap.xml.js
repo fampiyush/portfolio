02a9aaefe8317a894680128f05450cb3415aa300
Piyush Gupta <44369353+fampiyush@users.noreply.github.com>
2022-09-27 20:16:15 +0530

    updated sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = ({ res }) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
+<url>
+<loc>https://www.piyushg.com/</loc>
+<lastmod>2022-09-27T14:26:24+00:00</lastmod>
+</url>
+</urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
